b15e885b8e71ad5740fbe1248dd52cacaf4f183e
Rennan <rennansantos@discente.ufg.br>
2022-10-20 18:10:47 -0300

    feat(mapping/testing): create mock for auth and testing

--- tests/mapping/index.test.ts
+++ tests/mapping/index.test.ts
@@ -3,12 +3,32 @@
  */
 
 import { Mapping } from "../../src/core/mapping/index";
+import { Sara, Client } from "../../src/";
+import { ISession, Session } from "../../src/models/Session";
 
-jest.mock("../../src/index");
+let session: ISession;
+
+jest.mock("../../src/", () => {
+  return {
+    Sara: {
+      auth: jest.fn(() => {
+        const iSessionCreated: ISession = {
+          access_token: "access_token",
+          access_key: "access_key",
+          secret_key: "secret_key",
+          expires_in: 100000,
+          scope: "",
+        };
+        session = new Session(iSessionCreated);
+        return session;
+      }),
+    },
+  };
+});
 
 describe("testing mapping module", () => {
-  test("Connect a Mapping Process to a robot", async () => {
-    const mapping = new Mapping("144cdc28-9126-4f59-b32b-307b2ae43fb3");
-    expect(mapping).toBeDefined();
+  test("Authenticate Session", async () => {
+    const session = await Sara.auth("user", "pass");
+    expect(session.access_token).toBeDefined();
   });
 });
