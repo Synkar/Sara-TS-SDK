9021ee07207bd0f43d8c59952d17b6b3ab03a645
Rennan <rennansantos@discente.ufg.br>
2022-11-04 17:21:59 -0300

    fix(IAM): import all models to iam module

--- src/core/iam/index.ts
+++ src/core/iam/index.ts
@@ -5,6 +5,13 @@ import { DjangoDetail, IamSlugs } from "./models/Iam.models";
 
 import { Robots as _Robots } from "./Robots";
 import { Fleets as _Fleets } from "./Fleets";
+import { Users as _Users } from "./Users";
+import { Policies as _Policies } from "./Policies";
+import { Actions as _Actions } from "./Actions";
+import { Apps as _Apps } from "./Apps";
+import { Clients as _Clients } from "./Clients";
+import { Groups as _Groups } from "./Groups";
+import { Services as _Services } from "./Services";
 
 export class IAM {
   private resource = "iam";
@@ -48,6 +55,41 @@ export class IAM {
     return new _Fleets(session);
   } as any as { new (session?: ISession): any };
 
+  Users = function (session?: ISession) {
+    return new _Users(session);
+  } as any as { new (session?: ISession): any };
+
+  Policies = function (session?: ISession) {
+    return new _Policies(session);
+  } as any as { new (session?: ISession): any };
+
+  Actions = function (session?: ISession) {
+    return new _Actions(session);
+  } as any as { new (session?: ISession): any };
+
+  Apps = function (session?: ISession) {
+    return new _Apps(session);
+  } as any as { new (session?: ISession): any };
+
+  Clients = function (session?: ISession) {
+    return new _Clients(session);
+  } as any as { new (session?: ISession): any };
+
+  Groups = function (session?: ISession) {
+    return new _Groups(session);
+  } as any as { new (session?: ISession): any };
+
+  Services = function (session?: ISession) {
+    return new _Services(session);
+  } as any as { new (session?: ISession): any };
+
   static Robots = _Robots;
   static Fleets = _Fleets;
+  static Users = _Users;
+  static Policies = _Policies;
+  static Actions = _Actions;
+  static Apps = _Apps;
+  static Clients = _Clients;
+  static Groups = _Groups;
+  static Services = _Services;
 }
